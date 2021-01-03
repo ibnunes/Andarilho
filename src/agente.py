e5d8fe04ec2c88c2ef2198a5d8bc2e0e6fa8221f
Igor Nunes <thoga31@gmail.com>
2021-01-03 19:37:25 +0000

    Answers questions 1 and 2

--- src/agente.py
+++ src/agente.py
@@ -7,201 +7,200 @@ agente.py
 
 import time
 
+import networkx as nx
 
-class Node:
-    """
-    A node class for A* Pathfinding
-    parent is parent of the current Node
-    position is current position of the Node in the maze
-    g is cost from start to current Node
-    h is heuristic based estimated cost for current Node to end Node
-    f is total cost of present node i.e. :  f = g + h
-    """
 
-    def __init__(self, parent=None, position=None):
-        self.parent = parent
-        self.position = position
 
-        self.g = 0
-        self.h = 0
-        self.f = 0
+OBJ_NURSE   = 'enfermeiro'
+OBJ_DOCTOR  = 'medico'
+OBJ_PATIENT = 'doente'
+OBJ_TABLE   = 'mesa'
+OBJ_CHAIR   = 'cadeira'
+OBJ_BOOK    = 'livro'
+OBJ_BED     = 'cama'
+
+CATEGORY_OBJECT    = [OBJ_TABLE, OBJ_CHAIR, OBJ_BOOK, OBJ_BED]
+CATEGORY_PEOPLE    = [OBJ_NURSE, OBJ_DOCTOR, OBJ_PATIENT]
+CATEGORY_FURNITURE = [OBJ_BED, OBJ_CHAIR, OBJ_TABLE]
+CATEGORY_ALL       = CATEGORY_PEOPLE + CATEGORY_OBJECT
+
+SEPARATOR   = '_'
+
+
+ERROR_NOT_ENOUGH_PEOPLE = "Não foram encontradas pelo menos 2 pessoas até ao momento"
+
+
+
+class Log:
+    _flag = True
+
+    @staticmethod
+    def d(prompt):
+        if Log._flag:
+            print("[DEBUG] {0}".format(prompt))
+    
+    @staticmethod
+    def setMode(mode):
+        Log._flag = mode != 0
+
+
+class Things:
+    _list_people  = []
+    _list_objects = []
+
+    @staticmethod
+    def add(category, name):
+        if not Things.contains(category, name):
+            if category in CATEGORY_PEOPLE:
+                Things._list_people.append((category, name))
+            else:
+                Things._list_objects.append((category, name))
+    
+    @staticmethod
+    def contains(category, name):
+        return (category, name) in (Things._list_people + Things._list_objects)
+    
+    @staticmethod
+    def getLastButOnePerson():
+        if len(Things._list_people) >= 2:
+            return Things._list_people[-2]
+        else:
+            return ERROR_NOT_ENOUGH_PEOPLE
+    
+    @staticmethod
+    def getListOfPeople():
+        return Things._list_people
+    
+    @staticmethod
+    def getListOfObjects():
+        return Things._list_objects
+
+
+
+class Hospital:
+    _rooms = [
+        [( 30, 180), ( 30,  90)],   # Escadas
+        [( 85, 565), ( 30, 135)],   # Corredor 1
+        [( 30,  85), ( 90, 330)],   # Corredor 2
+        [(565, 635), ( 30, 330)],   # Corredor 3
+        [( 30, 770), (330, 410)],   # Corredor 4
+        [(130, 235), (180, 285)],   # Sala 5
+        [(280, 385), (180, 285)],   # Sala 6
+        [(430, 520), (180, 285)],   # Sala 7
+        [(680, 770), ( 30,  85)],   # Sala 8
+        [(680, 770), (130, 185)],   # Sala 9
+        [(680, 770), (230, 285)],   # Sala 10
+        [( 30, 235), (455, 770)],   # Sala 11
+        [(280, 385), (455, 770)],   # Sala 12
+        [(430, 570), (455, 770)],   # Sala 13
+        [(615, 770), (455, 770)],   # Sala 14
+    ]
+
+    _lastVisited = 0
+    _currentRoom = 0
+
+    _floor = nx.Graph()
+
+    @staticmethod
+    def getFloorGraph():
+        return Hospital._floor
+
+    @staticmethod
+    def updateFloor(newRoom):
+        if newRoom != 0:
+            if Hospital._currentRoom != newRoom:
+                Hospital._currentRoom, Hospital._lastVisited = Utils.swap(Hospital._currentRoom, newRoom)
+                Hospital._floor.add_edge(Hospital._currentRoom, Hospital._lastVisited)
+                Log.d("curr: {0}; last: {1}".format(Hospital._currentRoom, Hospital._lastVisited))
+
+    @staticmethod
+    def updateWithPosition(position):
+        if position == []:
+            return 0
+        else:
+            assert len(position) == 2
+            px, py = position[0], position[1]
+            for i in range(len(Hospital._rooms)):
+                rx, ry = Hospital._rooms[i][0], Hospital._rooms[i][1]
+                if Utils.inRange(px, rx) and Utils.inRange(py, ry):
+                    Hospital.updateFloor(i)
+                    return i
+            else:
+                return 0
+    
+    @staticmethod
+    def updateWithObjects(objects, position):
+        if objects != []:
+            position = tuple(position)
+            for obj in objects:
+                [category, name] = obj.split(SEPARATOR, 1)
+                if not Things.contains(category, name):
+                    try:
+                        currentObjects = list(map(lambda n: n[1], Hospital._floor.nodes[Hospital._currentRoom][category]))
+                        if name not in currentObjects:
+                            Hospital._floor.nodes[Hospital._currentRoom][category].append((position, name))
+                        # Log.d("Current objects of type {0} after append: {1}".format(category, currentObjects))
+                    except KeyError:
+                        Hospital._floor.nodes[Hospital._currentRoom][category] = [(position, name)]
+                        # Log.d("No objects found of type {0}. Added first with name {1}.".format(category, name))
+                    Things.add(category, name)
+    
+    @staticmethod
+    def getTypeOfRoom():
+        # Quarto: >= 1 cama
+        # Sala de enfermeiros: 0 camas, >= 1 cadeiras AND >= 1 mesas
+        # Sala de espera: > 2 cadeiras, 0 mesas, 0 camas
+        # Corredor: de 1 a 4
+        if Hospital._currentRoom in range(1, 5):
+            return "Corredor"
+
+        counter = {}
+        room = Hospital._floor.nodes[Hospital._currentRoom]
+        for category in CATEGORY_FURNITURE:
+            try:
+                counter[category] = len(room[category])
+            except KeyError:
+                counter[category] = 0
         
-    def __eq__(self, other):
-        return self.position == other.position
+        if counter[OBJ_BED] >= 1:
+            return "Quarto"
+        elif counter[OBJ_CHAIR] >= 1 and counter[OBJ_TABLE] >= 1:
+            return "Sala de enfermeiros"
+        elif counter[OBJ_CHAIR] > 2:
+            return "Sala de espera"
+        else:
+            return "Não tenho informação suficiente para determinar"
 
 
-#This function return the path of the search
-def return_path(current_node):
-    path = []
-    no_rows = 600
-    no_columns = 800 
+
+    @staticmethod
+    def getRoomIndex():
+        return Hospital._currentRoom
+
+
+class Utils:
+    @staticmethod
+    def inRange(n, r):
+        assert len(r) == 2
+        return n >= r[0] and n <= r[1]
     
-    # here we create the initialized result maze with -1 in every position
-    result = [[-1 for i in range(no_columns)] for j in range(no_rows)]
-    current = current_node
-    while current is not None:
-        path.append(current.position)
-        current = current.parent
-    # Return reversed path as we need to show from start to end path
-    path = path[::-1]
-    start_value = 0
-    # we update the path of start to end found by A-star serch with every step incremented by 1
-    for i in range(len(path)):
-        result[path[i][0]][path[i][1]] = start_value
-        start_value += 1
-    return result
+    @staticmethod
+    def swap(x, y):
+        return (y, x)
 
-
-def search(matrix1,cost, start, end):
-    """
-    Returns a list of tuples as a path from the given start to the given end in the given maze
-        :param maze:
-        :param cost
-        :param start:
-        :param end:
-        :return:
-    """
-
-    # Create start and end node with initized values for g, h and f
-    start_node = Node(None, tuple(start))
-    start_node.g = start_node.h = start_node.f = 0
-    end_node = Node(None, tuple(end))
-    end_node.g = end_node.h = end_node.f = 0
-
-    # Initialize both yet_to_visit and visited list
-    # in this list we will put all node that are yet_to_visit for exploration. 
-    # From here we will find the lowest cost node to expand next
-    yet_to_visit_list = []  
-    # in this list we will put all node those already explored so that we don't explore it again
-    visited_list = [] 
-    
-    # Add the start node
-    yet_to_visit_list.append(start_node)
-    
-    # Adding a stop condition. This is to avoid any infinite loop and stop 
-    # execution after some reasonable number of steps
-    outer_iterations = 0
-    max_iterations = 5000
-
-    # what squares do we search . serarch movement is left-right-top-bottom 
-    #(4 movements) from every positon
-
-# go up, left, down, right
-    move  =  [[0, -1 ], [ -1, 0], [ 0, 1 ], [ 1, 0 ]] # go 
-
-    """
-    1) We first get the current node by comparing all f cost and selecting the lowest cost node for further expansion
-    2) Check max iteration reached or not . Set a message and stop execution
-    3) Remove the selected node from yet_to_visit list and add this node to visited list
-    4) Perofmr Goal test and return the path else perform below steps
-    5) For selected node find out all children (use move to find children)
-        a) get the current postion for the selected node (this becomes parent node for the children)
-        b) check if a valid position exist (boundary will make few nodes invalid)
-        c) if any node is a wall then ignore that
-        d) add to valid children node list for the selected parent
-            
-        For all the children node
-            a) if child in visited list then ignore it and try next node
-            b) calculate child node g, h and f values
-            c) if child in yet_to_visit list then ignore it
-            d) else move the child to yet_to_visit list
-    """
-    #find maze has got how many rows and columns 
-    no_rows = 600
-    no_columns = 800
-    
-    # Loop until you find the end
-    
-    while len(yet_to_visit_list) > 0:
-        
-        # Every time any node is referred from yet_to_visit list, counter of limit operation incremented
-        outer_iterations += 1    
-
-        
-        # Get the current node
-        current_node = yet_to_visit_list[0]
-        current_index = 0
-        for index, item in enumerate(yet_to_visit_list):
-            if item.f < current_node.f:
-                current_node = item
-                current_index = index
-                
-        # if we hit this point return the path such as it may be no solution or 
-        # computation cost is too high
-        if outer_iterations > max_iterations:
-            return return_path(current_node)
-
-        # Pop current node out off yet_to_visit list, add to visited list
-        yet_to_visit_list.pop(current_index)
-        visited_list.append(current_node)
-
-        # test if goal is reached or not, if yes then return the path
-        if current_node == end_node:
-            return return_path(current_node)
-
-        # Generate children from all adjacent squares
-        children = []
-
-        for new_position in move: 
-            
-            # Get node position
-            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
-            
-            # Make sure within range (check if within maze boundary)
-            if (node_position[0] > (no_rows - 1) or node_position[0] < 0 or node_position[1] > (no_columns -1) or node_position[1] < 0):
-                continue
-
-            # Make sure walkable terrain
-            if matrix1[node_position[0]][node_position[1]] != 0:
-                continue
-
-            # Create new node
-            new_node = Node(current_node, node_position)
-
-            ## Append
-            children.append(new_node)
-
-        # Loop through children
-        for child in children:
-            
-            # Child is on the visited list (search entire visited list)
-            if len([visited_child for visited_child in visited_list if visited_child == child]) > 0:
-                continue
-
-            # Create the f, g, and h values
-            child.g = current_node.g + cost
-            # Heuristic costs calculated here, this is using eucledian distance
-            child.h = (((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)) 
-
-            child.f = child.g + child.h
-
-            # Child is already in the yet_to_visit list and g cost is already lower
-            if len([i for i in yet_to_visit_list if child == i and child.g > i.g]) > 0:
-                continue
-
-            # Add the child to the yet_to_visit list
-            yet_to_visit_list.append(child)
-
-
-divisao = [
-    [( 99,  14), ( 99, 150), (550,  14), (550, 150)],
-    [( 14,  75), (100,  75), ( 14, 315), (100, 315)],
-    [(550,  14), (550, 315), (650,  14), (650, 315)],
-    [( 14, 315), ( 14, 425), (785, 315), (785, 425)],
-    [(100, 149), (250, 149), (100, 315), (250, 315)],
-    [(250, 150), (250, 315), (400, 150), (400, 315)],
-    [(400, 150), (400, 315), (550, 150), (550, 315)],
-    [(649,  14), (649, 100), (785,  14), (785, 100)],
-    [(649, 100), (785, 100), (649, 200), (785, 200)],
-    [(649, 200), (785, 200), (649, 315), (785, 315)],
-    [( 14, 424), ( 14, 575), (250, 424), (250, 574)],
-    [(250, 424), (250, 575), (400, 424), (400, 575)],
-    [(400, 424), (400, 575), (590, 424), (590, 575)],
-    [(590, 424), (590, 575), (785, 424), (785, 575)] ]
 
 
 def work(posicao, bateria, objetos):
-    
+    # esta função é invocada em cada ciclo de clock
+    # e pode servir para armazenar informação recolhida pelo agente
+    # recebe:
+    # posicao = a posição atual do agente, uma lista [X,Y]
+    # bateria = valor de energia na bateria, um número inteiro >= 0
+    # objetos = o nome do(s) objeto(s) próximos do agente, uma string
+
+    # podem achar o tempo atual usando, p.ex.
+    # time.time()
+
     global pos_x
     global pos_y
     
@@ -209,30 +208,56 @@ def work(posicao, bateria, objetos):
     #posicao1.extend(posicao)
     pos_x = posicao[0]
     pos_y = posicao[1]
+
+    Hospital.updateWithPosition(posicao)
+    Hospital.updateWithObjects(objetos, posicao)
+    # Log.d("Posicao = [{0}, {1}]; Bateria = {2:.2f}; Objetos = {3}; Sala = {4}".format(pos_x, pos_y, bateria, objetos, Hospital.getRoomIndex()))
     
 
 
 def resp1():
-    pass
+    # Qual foi a penúltima pessoa que viste?
+    # Log.d("People: {0}\n Objects: {1}".format(Things.getListOfPeople(), Things.getListOfObjects()))
+    print("Resposta: {0}".format(Things.getLastButOnePerson()))
+
 
 def resp2():
-    pass
-                    
+    # Em que tipo de sala estás agora?
+    # Log.d("Nodes: {0}\nEdges: {1}".format(Hospital.getFloorGraph().nodes(data=True), Hospital.getFloorGraph().edges()))
+    print("Resposta: {0}".format(Hospital.getTypeOfRoom()))
+
 
 def resp3():
+    # Qual o caminho para a sala de enfermeiros mais próxima?
+    print("Pergunta 3")
     pass
-    
+
+
 def resp4():
+    # Qual a distância até ao médico mais próximo?
+    print("Pergunta 4")
     pass
-    
+
+
 def resp5():
+    # Quanto tempo achas que demoras a ir de onde estás até às escadas?
+    print("Pergunta 5")
     pass
+
 
 def resp6():
+    # Quanto tempo achas que falta até ficares sem bateria?
+    print("Pergunta 6")
     pass
+
 
 def resp7():
+    # Qual a probabilidade de encontrar um livro numa divisão se já encontraste uma cadeira?
+    print("Pergunta 7")
     pass
 
+
 def resp8():
+    # Se encontrares um enfermeiro numa divisão, qual é a probabilidade de estar lá um doente?
+    print("Pergunta 8")
     pass
