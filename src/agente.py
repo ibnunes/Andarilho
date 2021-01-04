01a9501c4cec06edd7b60117f2ef98885757d357
Igor Nunes <thoga31@gmail.com>
2021-01-04 00:44:01 +0000

    Answers question 3

--- src/agente.py
+++ src/agente.py
@@ -24,7 +24,25 @@ CATEGORY_PEOPLE    = [OBJ_NURSE, OBJ_DOCTOR, OBJ_PATIENT]
 CATEGORY_FURNITURE = [OBJ_BED, OBJ_CHAIR, OBJ_TABLE]
 CATEGORY_ALL       = CATEGORY_PEOPLE + CATEGORY_OBJECT
 
-SEPARATOR   = '_'
+SEPARATOR = '_'
+
+MAP_MIDPOINT = 'mid'
+MAP_DISTANCE = 'weight'
+MAP_ROBOT    = 'X'
+
+ROOM_UNKNOWN  = 0
+ROOM_CORRIDOR = 1
+ROOM_BEDROOM  = 2
+ROOM_NURSES   = 3
+ROOM_WAITING  = 4
+
+ROOM_DESCRIPTION = [
+    "Não tenho informação suficiente para determinar",
+    "Corredor",
+    "Quarto",
+    "Sala de enfermeiros",
+    "Sala de espera"
+]
 
 
 ERROR_NOT_ENOUGH_PEOPLE = "Não foram encontradas pelo menos 2 pessoas até ao momento"
@@ -77,6 +95,19 @@ class Things:
 
 
 
+class Robot:
+    _lastPos = (0, 0)
+    _currPos = (0, 0)
+
+    @staticmethod
+    def setPosition(x, y):
+        Robot._currPos, Robot._lastPos = Utils.swap(Robot._currPos, (x, y))
+    
+    @staticmethod
+    def getPosition():
+        return Robot._currPos
+
+
 class Hospital:
     _rooms = [
         [( 30, 180), ( 30,  90)],   # Escadas
@@ -100,10 +131,76 @@ class Hospital:
     _currentRoom = 0
 
     _floor = nx.Graph()
+    _map = nx.Graph()
+
+
+    @staticmethod
+    def getRoomMidPoint(room):
+        return Utils.midpoint(Hospital._rooms[room][0], Hospital._rooms[room][1])
+
 
     @staticmethod
     def getFloorGraph():
         return Hospital._floor
+    
+
+    @staticmethod
+    def getMapGraph():
+        return Hospital._map
+    
+
+    @staticmethod
+    def roomToStr(r):
+        return "R{0:02d}".format(r)
+
+
+    @staticmethod
+    def doorToStr(r1, r2):
+        return "D{0:02d}{1:02d}".format(min(r1, r2), max(r1, r2))
+    
+
+    @staticmethod
+    def getEdgeBetweenRoomAndDoor(room_src, room_dest):
+        return (Hospital.roomToStr(room_src), Hospital.doorToStr(room_src, room_dest))
+    
+
+    @staticmethod
+    def getEdgeBetweenDoorAndDoor(r1, r2, r3):
+        return (Hospital.doorToStr(r1, r2), Hospital.doorToStr(r1, r3))
+
+    
+    @staticmethod
+    def computeDirectDoorPaths():
+        for r in Hospital._floor.nodes():
+            rooms = sorted(list(nx.all_neighbors(Hospital._floor, r)))
+            # Log.d("room={0}; neighbors={1}".format(r, rooms))
+            for i in range(0, len(rooms)-1):
+                for j in range(i+1, len(rooms)):
+                    edge = Hospital.getEdgeBetweenDoorAndDoor(r, rooms[i], rooms[j])
+                    if not Hospital._map.has_edge(*edge):
+                        door_i = Hospital._map.nodes[Hospital.doorToStr(r, rooms[i])][MAP_MIDPOINT]
+                        door_j = Hospital._map.nodes[Hospital.doorToStr(r, rooms[j])][MAP_MIDPOINT]
+                        distance = Utils.distance(door_i, door_j)
+                        Hospital._map.add_edges_from([edge], weight=distance)
+    
+
+    @staticmethod
+    def updateMap():
+        cr, lv, rpos = Hospital._currentRoom, Hospital._lastVisited, Robot.getPosition()
+
+        midpoint = Hospital.getRoomMidPoint(cr)
+        distance = Utils.distance(midpoint, rpos)
+        Hospital._map.add_edges_from([Hospital.getEdgeBetweenRoomAndDoor(cr, lv)], weight=distance)
+        Hospital._map.nodes[Hospital.roomToStr(cr)][MAP_MIDPOINT] = midpoint
+
+        midpoint = Hospital.getRoomMidPoint(lv)
+        distance = Utils.distance(midpoint, rpos)
+        Hospital._map.add_edges_from([Hospital.getEdgeBetweenRoomAndDoor(lv, cr)], weight=distance)
+        Hospital._map.nodes[Hospital.roomToStr(lv)][MAP_MIDPOINT] = midpoint
+
+        Hospital._map.nodes[Hospital.doorToStr(cr, lv)][MAP_MIDPOINT] = rpos
+        Hospital.computeDirectDoorPaths()
+
 
     @staticmethod
     def updateFloor(newRoom):
@@ -111,7 +208,26 @@ class Hospital:
             if Hospital._currentRoom != newRoom:
                 Hospital._currentRoom, Hospital._lastVisited = Utils.swap(Hospital._currentRoom, newRoom)
                 Hospital._floor.add_edge(Hospital._currentRoom, Hospital._lastVisited)
-                Log.d("curr: {0}; last: {1}".format(Hospital._currentRoom, Hospital._lastVisited))
+                Hospital.updateMap()
+                # Log.d("Floor: curr={0}; last={1}".format(Hospital._currentRoom, Hospital._lastVisited))
+    
+
+    @staticmethod
+    def addRobotToMap():
+        Hospital._map.add_node(MAP_ROBOT)
+        edges = nx.all_neighbors(Hospital._map, Hospital.roomToStr(Hospital._currentRoom))
+        for e in edges:
+            distance = Utils.distance(Hospital._map.nodes[e][MAP_MIDPOINT], Robot.getPosition())
+            Hospital._map.add_edge(MAP_ROBOT, e, weight=distance)
+    
+
+    @staticmethod
+    def removeRobotFromMap():
+        try:
+            Hospital._map.remove_node(MAP_ROBOT)
+        except:
+            pass
+
 
     @staticmethod
     def updateWithPosition(position):
@@ -145,31 +261,43 @@ class Hospital:
                         # Log.d("No objects found of type {0}. Added first with name {1}.".format(category, name))
                     Things.add(category, name)
     
+
     @staticmethod
-    def getTypeOfRoom():
+    def roomDescription(room_code):
+        return ROOM_DESCRIPTION[room_code]
+
+
+    @staticmethod
+    def getTypeOfRoom(room):
         # Quarto: >= 1 cama
         # Sala de enfermeiros: 0 camas, >= 1 cadeiras AND >= 1 mesas
         # Sala de espera: > 2 cadeiras, 0 mesas, 0 camas
         # Corredor: de 1 a 4
-        if Hospital._currentRoom in range(1, 5):
-            return "Corredor"
+        if room in range(1, 5):
+            return ROOM_CORRIDOR
 
         counter = {}
-        room = Hospital._floor.nodes[Hospital._currentRoom]
+        room_data = Hospital._floor.nodes[room]
         for category in CATEGORY_FURNITURE:
             try:
-                counter[category] = len(room[category])
+                counter[category] = len(room_data[category])
             except KeyError:
                 counter[category] = 0
         
         if counter[OBJ_BED] >= 1:
-            return "Quarto"
+            return ROOM_BEDROOM
         elif counter[OBJ_CHAIR] >= 1 and counter[OBJ_TABLE] >= 1:
-            return "Sala de enfermeiros"
+            return ROOM_NURSES
         elif counter[OBJ_CHAIR] > 2:
-            return "Sala de espera"
+            return ROOM_WAITING
         else:
-            return "Não tenho informação suficiente para determinar"
+            return ROOM_UNKNOWN
+    
+
+    @staticmethod
+    def getCurrentTypeOfRoom():
+        return Hospital.getTypeOfRoom(Hospital._currentRoom)
+
 
     @staticmethod
     def getDistanceToNearestDoctor():
@@ -178,11 +306,33 @@ class Hospital:
             if OBJ_DOCTOR in things:
                 doctors += list(map(lambda t: (room, t[0], t[1]), things[OBJ_DOCTOR]))
         if len(doctors) > 0:
-            doctors = list(map(lambda d: (d[0], Utils.distance(d[1], (pos_x, pos_y)), d[2]), doctors))
+            doctors = list(map(lambda d: (d[0], Utils.distance(d[1], Robot.getPosition()), d[2]), doctors))
             doctors.sort(key = lambda d: d[1])
             return "Médico {0} na sala {1} a uma distância de {2:.3f}.".format(doctors[0][2], doctors[0][0], doctors[0][1])
         else:
             return "Ainda não encontrei médicos"
+    
+
+    @staticmethod
+    def getPathToNearestNurseOffice():
+        nurse_rooms = list(filter(lambda n: Hospital.getTypeOfRoom(n) == ROOM_NURSES, Hospital._floor.nodes()))
+        result = []
+        if len(nurse_rooms) > 0:
+            if Hospital.getTypeOfRoom(Hospital._currentRoom) == ROOM_NURSES:
+                return [Hospital.roomToStr(Hospital._currentRoom)]
+            Hospital.addRobotToMap()
+            for r in nurse_rooms:
+                try:
+                    path   = nx.astar_path(Hospital._map, MAP_ROBOT, Hospital.roomToStr(r))
+                    weight = nx.astar_path_length(Hospital._map, MAP_ROBOT, Hospital.roomToStr(r))
+                    result.append((weight, path))
+                except nx.NetworkXNoPath:
+                    continue
+            result.sort(key = lambda t: t[0])
+            Hospital.removeRobotFromMap()
+            return result[0][1]
+        else:
+            return []
 
 
     @staticmethod
@@ -203,6 +353,10 @@ class Utils:
     @staticmethod
     def distance(a, b):
         return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** 0.5
+    
+    @staticmethod
+    def midpoint(a, b):
+        return ((a[0] + a[1]) // 2, (b[0] + b[1]) // 2)
 
 
 
@@ -217,35 +371,29 @@ def work(posicao, bateria, objetos):
     # podem achar o tempo atual usando, p.ex.
     # time.time()
 
-    global pos_x
-    global pos_y
-    
-    #copia as posicoes para o posicao1 para podermos utilizar nas questoes
-    pos_x = posicao[0]
-    pos_y = posicao[1]
+    Robot.setPosition(posicao[0], posicao[1])
 
-    Hospital.updateWithPosition(posicao)
-    Hospital.updateWithObjects(objetos, posicao)
+    Hospital.updateWithPosition(Robot.getPosition())
+    Hospital.updateWithObjects(objetos, Robot.getPosition())
     # Log.d("Posicao = [{0}, {1}]; Bateria = {2:.2f}; Objetos = {3}; Sala = {4}".format(pos_x, pos_y, bateria, objetos, Hospital.getRoomIndex()))
-    
 
 
 def resp1():
     # Qual foi a penúltima pessoa que viste?
     # Log.d("People: {0}\n Objects: {1}".format(Things.getListOfPeople(), Things.getListOfObjects()))
+    # Log.d("Nodes: {0}\nEdges: {1}".format(Hospital.getMapGraph().nodes(data=True), Hospital.getMapGraph().edges(data=True)))
     print("Resposta: {0}".format(Things.getLastButOnePerson()))
 
 
 def resp2():
     # Em que tipo de sala estás agora?
     # Log.d("Nodes: {0}\nEdges: {1}".format(Hospital.getFloorGraph().nodes(data=True), Hospital.getFloorGraph().edges()))
-    print("Resposta: {0}".format(Hospital.getTypeOfRoom()))
+    print("Resposta: {0}".format(Hospital.roomDescription(Hospital.getCurrentTypeOfRoom())))
 
 
 def resp3():
     # Qual o caminho para a sala de enfermeiros mais próxima?
-    # Euclidiana, por grafos ou por A*?
-    print("Pergunta 3")
+    print("Resposta: {0}".format(Hospital.getPathToNearestNurseOffice()))
     pass
 
 
